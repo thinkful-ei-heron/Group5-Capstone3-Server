5034065ac17340baf4d8c64ae3b766f681da3dd8
Michael Kirsch <michaelskirsch@gmail.com>
2020-01-08 16:51:46 -0700

    UNTESTED (end of day) handling for full bookmark structure

--- src/user/user-service.js
+++ src/user/user-service.js
@@ -18,23 +18,113 @@ checker.disallowPasswords(true, true, 4);
 
 const UserService = {
   getListIds(db, user_id) {
-    return db.from('userlist').whereIn('user_id', user_id);
+    console.log('getListIds');
+    return db
+      .pluck('list_id')
+      .from('userlist')
+      .where('user_id', user_id);
   },
   getFolderIds(db, list_id) {
-    return db.from('listfolder').whereIn('list_id', list_id);
+    console.log('getFolderIds');
+    return db
+      .pluck('folder_id')
+      .from('listfolder')
+      .whereIn('list_id', list_id);
   },
   getBookmarkIds(db, folder_id) {
-    return db.from('folderbookmarks').whereIn('folder_id', folder_id);
+    console.log('getBookmarkIds');
+    return db
+      .pluck('bookmark_id')
+      .from('folderbookmarks')
+      .whereIn('folder_id', folder_id);
   },
   getBookmarks(db, bookmark_ids) {
+    console.log('getBookmarks');
     return db.from('bookmarks').whereIn('id', bookmark_ids);
   },
   getFolders(db, folder_ids) {
+    console.log('getFolders');
     return db.from('folders').whereIn('id', folder_ids);
   },
   getLists(db, list_ids) {
+    console.log('getLists');
     return db.from('lists').whereIn('id', list_ids);
   },
+
+  async makeBookmarkStructure(db, list_id) {
+    const folderStructure = await this.mapFolderStructureForList(db, list_id);
+    const bookmarks = await this.getBookmarksWithParents(db, list_id);
+    this.addBookmarksToFolders(folderStructure, bookmarks);
+    return folderStructure;
+  },
+  async mapFolderStructureForList(db, list_id) {
+    const folderIds = await db
+      .pluck('folder_id')
+      .from('listfolder')
+      .where({ folder_id });
+    const folders = await db
+      .select('*') //id, parent_id, name
+      .from('folders')
+      .whereIn('id', folderIds);
+
+    const folderStructure = this.makeFolderStructure(folders);
+  },
+  async getBookmarksWithParents(db, list_id) {
+    const folderIds = await this.getFolderIds(db, list_id);
+    const folders = await this.getFolders(db, folderIds);
+    return await folders.map(async folder => {
+      const bookmarkIdsInFolder = await this.getBookmarkIds(db, [folder]);
+      const bookmarks = await this.getBookmarks(
+        db,
+        bookmarkIdsInFolder
+      ).map(bookmark => ({ ...bookmark, folder_id: folder.id }));
+      return bookmarks;
+    });
+  },
+  makeFolderStructure(folders) {
+    //array of folders
+    const subfolders = {};
+    for (const folder of folders) {
+      const parentId = folder.parent_id;
+      if (!subfolders[parentId]) {
+        subfolders[parentId] = [];
+      }
+      subfolders[parentId].push(folder);
+    }
+    //subfolders is an object with keys that are parent IDs and values that are arrays of all subfolders
+    for (let folder of folders) {
+      folder.contents = subfolders[folder.id] || [];
+    }
+    const rootFolder = {
+      name: '',
+      id: 0,
+      contents: []
+    };
+    rootFolder.contents = subfolders[0];
+    return rootFolder;
+  },
+
+  addBookmarksToFolders(folderStructure, bookmarkArray) {
+    //should modify in place
+    const bookmarksByFolder = {};
+    for (const bookmark of bookmarkArray) {
+      const folderId = bookmark.folder_id;
+      if (!bookmarksByFolder[folderId]) {
+        bookmarksByFolder[folderId] = [];
+      }
+      bookmarksByFolder[folderId].push(bookmark);
+    }
+    recursiveAdd(folderStructure, bookmarksByFolder);
+  },
+
+  recursiveAdd(folder, foo) {
+    //should modify in place
+    for (const subfolder of folder.contents) {
+      recursiveAdd(subfolder, foo);
+    }
+    folder.contents = folder.contents.concat(foo[folder.id]);
+  },
+
   hasUserWithUserName(db, username) {
     return db('users')
       .where({ username })
