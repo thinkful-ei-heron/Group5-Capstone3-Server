c9445d1d2204de61ce304398404d2a209d959e9a
Michael Kirsch <michaelskirsch@gmail.com>
2020-01-09 15:35:35 -0700

    update seed to add ordering

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -44,20 +44,20 @@ VALUES
   (1, 1);
 
 
-INSERT INTO listnode (list_id, node_id)
+INSERT INTO listnode (list_id, node_id, next_node)
 VALUES 
-  (1, 1),
-  (1, 2),
-  (1, 3),
-  (1, 4),
-  (1, 5),
-  (1, 6),
-  (1, 7),
-  (1, 8),
-  (1, 9),
-  (1, 10),
-  (1, 11),
-  (1, 12);
+  (1, 1, 2),
+  (1, 2, 3),
+  (1, 3, 4),
+  (1, 4, 5),
+  (1, 5, 6),
+  (1, 6, 7),
+  (1, 7, 8),
+  (1, 8, 9),
+  (1, 9, 10),
+  (1, 10, 11),
+  (1, 11, 12),
+  (1, 12, null);
 
 
 INSERT INTO nodetag (node_id, tag_id)
